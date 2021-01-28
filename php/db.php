ab53770f875348199232c27a0dcf4fc04018fb8b
Shubham <67864186+shubhamgogri@users.noreply.github.com>
2021-01-28 21:25:03 +0530

    php file

---
+++ php/db.php
@@ -0,0 +1,31 @@
+<?php 
+
+$host = "localhost";
+$dbusername = "root";
+$dbPassword = "shubham_yes";
+$dbname = "login";
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];	
+$pass = $_POST['pass'];
+
+echo "Successfully";
+
+if (!empty($firstname) &&!empty($lastname)&& !empty($email) && !empty($pass)) {
+  
+	$con = new mysqli($host,$dbusername,$dbPassword,$dbname);
+
+	$Sql_Query = "insert into 'registration app' (First Name,Last Name, e-mail, password) values ('$firstname','$lastname','$email',$pass)";
+
+	if(mysqli_query($con,$Sql_Query)){
+
+	echo 'Data Submit Successfully';
+	}else{
+	echo 'Try Again';
+	}
+	mysqli_close($con);
+  }else{
+	echo "string";
+}
+ ?>
